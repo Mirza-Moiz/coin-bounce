5482c02d61a0ba26db93cce3acddc568036b1af6
Mirza-Moiz <moiznadeem1243@gmail.com>
2023-08-03 22:44:31 +0500

    Defined Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,85 @@ function App() {
             <Routes>
               <Route path="/" exact element={<div className={styles.main}>
                 < Home /></div>} />
+              <Route
+                path="crypto"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Crypto Page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="blogs"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Blogs Page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="blog/:id"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Blog Page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="blog-update/:id"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Update Blog Page</h1>
+                  </div>
+
+                }
+              />
+
+              <Route
+                path="submit"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Add Blog Page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="signup"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>SignUp page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="login"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <h1>Login page</h1>
+                  </div>
+                }
+              />
+
+              <Route
+                path="*"
+                element={
+                  <div className={styles.main}>
+                    <h1>404 Not Found</h1>
+                  </div>
+                }
+              />
             </Routes>
           </div>
           <Footer />
