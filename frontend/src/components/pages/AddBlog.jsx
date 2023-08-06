8030a42ec0fba195116d685c94dc2f02eb68e5f8
Mirza-Moiz <moiznadeem1243@gmail.com>
2023-08-06 23:45:29 +0500

    Added: pages

--- frontend/src/components/pages/AddBlog.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AddBlog() {
-  return <div>AddBlog</div>;
-}
-
-export default AddBlog;
---
+++ frontend/src/components/pages/Blog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Blog() {
+  return <div>Blogs</div>;
+}
+
+export default Blog;
