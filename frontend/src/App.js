d5c4b70166d158cd449dcc77ac19658d6a0a9241
Mirza-Moiz <moiznadeem1243@gmail.com>
2023-08-09 17:14:43 +0500

    Added: custom hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/pages/Home/Home';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.container}>
       <BrowserRouter>
         <div className={styles.layout}>
-
           <Navbar />
           <div>
-
             <Routes>
-              <Route path="/" exact element={<div className={styles.main}>
-                < Home /></div>} />
+              <Route
+                path="/"
+                exact
+                element={
+                  <div className={styles.main}>
+                    <Home />
+                  </div>
+                }
+              />
               <Route
                 path="crypto"
                 exact
