1f7074bf2b5e74496de11c46f9382088176f81d2
Mirza-Moiz <moiznadeem1243@gmail.com>
2023-08-10 22:36:36 +0500

    Added: Schemas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/pages/Home/Home";
+import Home from './pages/Home/Home';
 import Footer from "./components/Footer/Footer";
 
 import styles from "./App.module.css";
