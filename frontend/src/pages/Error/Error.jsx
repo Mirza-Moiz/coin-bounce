84e23b59cc00d72bcb582cfcc16749293514ef15
Mirza-Moiz <moiznadeem1243@gmail.com>
2023-08-12 23:40:54 +0500

    Updated: Error page

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import styles from "./Error.module.css";
+import { Link } from "react-router-dom";
 
-const Error = () => {
-  return <div>Error</div>;
-};
+function Error() {
+  return (
+    <div className={styles.errorWrapper}>
+      <div className={styles.errorHeader}>Error 404 - Page not found</div>
+      <div className={styles.errorBody}>
+        Go back to
+        <Link to="/" className={styles.homeLink}>
+          home
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default Error;
